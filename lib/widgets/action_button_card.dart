dabf1fbe2bc4ca2ef1e8e1b68ee110bd01720a8c
César Polanco <cpolcam@outlook.com>
2025-09-26 21:32:42 -0600

    fix web again

--- lib/widgets/action_button_card.dart
+++ lib/widgets/action_button_card.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../core/theme/colors.dart';
 import '../core/theme/text_styles.dart';
 
@@ -23,38 +23,27 @@ class ActionButtonCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final screenWidth = MediaQuery.of(context).size.width;
-    final isWeb = kIsWeb;
-    final isMobile = screenWidth < 768;
-
-    // Responsive values based on screen size
-    final cardHeight = isWeb ? 100.0 : (isMobile ? 120.0 : 110.0);
-    final iconContainerSize = isWeb ? 40.0 : (isMobile ? 48.0 : 44.0);
-    final iconSize = isWeb ? 20.0 : (isMobile ? 24.0 : 22.0);
-    final fontSize = isWeb ? 14.0 : (isMobile ? 14.0 : 16.0);
-    final horizontalMargin = isWeb ? 4.0 : (isMobile ? 4.0 : 6.0);
-
     return Expanded(
       child: Container(
-        margin: EdgeInsets.symmetric(horizontal: horizontalMargin),
+        margin: EdgeInsets.symmetric(horizontal: 4.w),
         child: Material(
           color: Colors.transparent,
           child: InkWell(
             onTap: enabled ? onTap : null,
-            borderRadius: BorderRadius.circular(12),
+            borderRadius: BorderRadius.circular(12.r),
             child: Container(
-              height: cardHeight,
+              height: 120.h,
               decoration: BoxDecoration(
                 color: enabled
-                    ? (backgroundColor ?? (kIsWeb ? Colors.red : cardColor))
+                    ? (backgroundColor ?? cardColor)
                     : cardColor.withOpacity(0.5),
-                borderRadius: BorderRadius.circular(12),
+                borderRadius: BorderRadius.circular(12.r),
                 boxShadow: [
                   if (enabled)
                     BoxShadow(
                       color: Colors.black.withOpacity(0.08),
-                      offset: const Offset(0, 2),
-                      blurRadius: 8,
+                      offset: Offset(0, 2.h),
+                      blurRadius: 8.r,
                       spreadRadius: 0,
                     ),
                 ],
@@ -67,42 +56,32 @@ class ActionButtonCard extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Container(
-                    width: iconContainerSize,
-                    height: iconContainerSize,
+                    width: 48.w,
+                    height: 48.h,
                     decoration: BoxDecoration(
                       color: enabled
                           ? (iconColor ?? primaryColor).withOpacity(0.1)
                           : dividerColor.withOpacity(0.3),
-                      borderRadius: BorderRadius.circular(iconContainerSize / 2),
+                      borderRadius: BorderRadius.circular(24.r),
                     ),
-                    child: kIsWeb
-                        ? Text(
-                            "◉", // Simple circle character
-                            style: TextStyle(
-                              fontSize: iconSize,
-                              color: enabled
-                                  ? (iconColor ?? primaryColor)
-                                  : textSecondary,
-                            ),
-                          )
-                        : Icon(
-                            icon,
-                            size: iconSize,
-                            color: enabled
-                                ? (iconColor ?? primaryColor)
-                                : textSecondary,
-                          ),
+                    child: Icon(
+                      icon,
+                      size: 24.w,
+                      color: enabled
+                          ? (iconColor ?? primaryColor)
+                          : textSecondary,
+                    ),
                   ),
-                  const SizedBox(height: 8),
+                  SizedBox(height: 8.h),
                   Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    padding: EdgeInsets.symmetric(horizontal: 8.w),
                     child: Text(
                       title,
                       textAlign: TextAlign.center,
                       style: subtitle2.copyWith(
                         color: enabled ? textPrimary : textSecondary,
                         fontWeight: FontWeight.w500,
-                        fontSize: fontSize,
+                        fontSize: 14.sp,
                       ),
                       maxLines: 1,
                       overflow: TextOverflow.ellipsis,
